805dc6c9f46d776a80c35a845245f0f289f041ef
Arjun Biju <arjunbiju332@gmail.com>
2024-06-26 16:39:00 +0530

    template design in progress

--- myMovie/settings.py
+++ myMovie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-xdl1mud*=@2@4+6eaimh%633&h2=sa&gd#6semb^^waq+og38u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
